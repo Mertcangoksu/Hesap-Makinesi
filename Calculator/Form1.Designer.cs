01dcfb92ca18a0f2f25c302073636302ff2b3329
mertcangoksu <117410814+Mertcangoksu@users.noreply.github.com>
2022-12-25 13:17:18 +0300

    Başlangıç Hesap Makinesi C#

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(288, 106);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(135, 38);
+            this.txtSayi1.TabIndex = 0;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(429, 106);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(135, 38);
+            this.txtSayi2.TabIndex = 0;
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Location = new System.Drawing.Point(322, 181);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(101, 32);
+            this.btnTopla.TabIndex = 1;
+            this.btnTopla.Text = "+";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.Location = new System.Drawing.Point(429, 181);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(101, 32);
+            this.btnEksi.TabIndex = 1;
+            this.btnEksi.Text = "-";
+            this.btnEksi.UseVisualStyleBackColor = true;
+            this.btnEksi.Click += new System.EventHandler(this.btnEksi_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Location = new System.Drawing.Point(429, 219);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(101, 32);
+            this.btnBol.TabIndex = 1;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Location = new System.Drawing.Point(322, 219);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(101, 32);
+            this.btnCarp.TabIndex = 1;
+            this.btnCarp.Text = "*";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 424);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnEksi);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnEksi;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Button btnCarp;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(txtSayi1.Text);
+            double sayi2=Convert.ToDouble(txtSayi2.Text);
+            string sonuc = (sayi1 + sayi2).ToString();
+            MessageBox.Show(sonuc);
+        }
+
+        private void btnEksi_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(txtSayi1.Text);
+            double sayi2 = Convert.ToDouble(txtSayi2.Text);
+            string sonuc = (sayi1 - sayi2).ToString();
+            MessageBox.Show(sonuc);
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(txtSayi1.Text);
+            double sayi2 = Convert.ToDouble(txtSayi2.Text);
+            string sonuc = (sayi1 * sayi2).ToString();
+            MessageBox.Show(sonuc);
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(txtSayi1.Text);
+            double sayi2 = Convert.ToDouble(txtSayi2.Text);
+            string sonuc = (sayi1 / sayi2).ToString();
+            MessageBox.Show(sonuc);
+        }
+    }
+}
